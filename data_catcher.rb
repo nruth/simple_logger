08299a2b04abad85c2fd9029a417d16470949633
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-05-22 15:58:38 +0200

    ensure key exists before read

--- data_catcher.rb
+++ data_catcher.rb
@@ -3,13 +3,14 @@ require 'sinatra'
 set :cache, Dalli::Client.new
 
 get '/log' do
+  settings.cache.add 'log', ''
   @log_data = settings.cache.get('log')
   erb :log
 end
 
 get '/log/:data' do |data|
   written = false
-  settings.cache.add 'log', ''
+  ensure_key_exists
   until written do
     written = settings.cache.cas('log') { |log| log + data }
   end
@@ -19,4 +20,8 @@ end
 get '/reset' do
   settings.cache.delete 'log'
   'cleaned'
+end
+
+def ensure_key_exists
+  settings.cache.add 'log', ''
 end
