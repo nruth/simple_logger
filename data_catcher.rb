37ace9521bbace6cb8b7b93e42f8a6718462d0f2
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-05-22 15:19:07 +0200

    move log to /log/ path

--- data_catcher.rb
+++ data_catcher.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 set :cache, Dalli::Client.new
 
-get '/:data' do |data|
+get '/log/:data' do |data|
   log = settings.cache.get('log') || ''
   log << data
   settings.cache.set 'log', log
