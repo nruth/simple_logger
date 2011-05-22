dd7bbe6b51e3b4e1b3d8b49f491e5c2aa8a58ccd
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-05-22 15:57:20 +0200

    view template for log

--- data_catcher.rb
+++ data_catcher.rb
@@ -3,7 +3,8 @@ require 'sinatra'
 set :cache, Dalli::Client.new
 
 get '/log' do
-  javascript + settings.cache.get('log')
+  @log_data = settings.cache.get('log')
+  erb :log
 end
 
 get '/log/:data' do |data|
@@ -18,41 +19,4 @@ end
 get '/reset' do
   settings.cache.delete 'log'
   'cleaned'
-end
-
-def javascript
-<<-JS
-<script language="JavaScript"> 
-// Configure refresh interval (in seconds) 
-var refreshinterval=5 
-// Shall the coundown be displayed inside your status bar? Say "yes" 
-or "no" below: 
-var displaycountdown="yes" 
-// Do not edit the code below 
-var starttime 
-var nowtime 
-var reloadseconds=0 
-var secondssinceloaded=0 
-function starttime() { 
-  starttime=new Date() 
-  starttime=starttime.getTime() 
-  countdown() 
-} 
-function countdown() { 
-  nowtime= new Date() 
-  nowtime=nowtime.getTime() 
-  secondssinceloaded=(nowtime-starttime)/1000 
-  reloadseconds=Math.round(refreshinterval-secondssinceloaded) 
-  if (refreshinterval>=secondssinceloaded) { 
-    var timer=setTimeout("countdown()",1000) 
-    if (displaycountdown=="yes") { 
-      window.status="Page refreshing in "+reloadseconds+ " seconds" 
-    } 
-  } else { 
-    clearTimeout(timer) window.location.reload(true) 
-  } 
-} 
-window.onload=starttime 
-</script>
-JS
 end
