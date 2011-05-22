b62e17099e7f7d31e01ef1b65da5771a8e9e7358
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-05-22 15:12:04 +0200

    start with empty string

--- data_catcher.rb
+++ data_catcher.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :cache, Dalli::Client.new
 
 get '/:data' do |data|
-  log = settings.cache.get 'log'
+  log = settings.cache.get('log') || ''
   log << data
   settings.cache.set 'log', log
   log
